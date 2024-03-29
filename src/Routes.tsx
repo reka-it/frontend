03adc691bc4bf703f5cce99e2e8fecc6c4789ce2
aTrueYety <erikhoff00@gmail.com>
2024-03-29 18:47:21 +0100

    Setup routes

---
+++ src/Routes.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home';
+
+const AppRoutes: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>Hello REKAAA</div>
+  );
+}
+
+export default App;
